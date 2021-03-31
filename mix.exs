b2f6764c4e2c33211e99187edfa00a42846ac21c
Norberto Oliveira Junior <norbejunior@gmail.com>
2021-03-31 00:04:38 -0300

    Add ex_doc and config package metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Machinist.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/norbajunior/machinist"
+
   def project do
     [
       app: :machinist,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @repo_url,
+
+      # Hex
+      package: package(),
+      description: "A tiny Elixir lib to write state machines"
     ]
   end
 
@@ -21,8 +28,14 @@ defmodule Machinist.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
     ]
   end
 end
