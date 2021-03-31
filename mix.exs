8eca8cb81ca5f8e3d69fdcb1cd5cda1a8cf0047f
Norberto Oliveira Junior <norbejunior@gmail.com>
2021-03-30 23:41:18 -0300

    Update workflow

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Machinist.MixProject do
     [
       app: :machinist,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
