53d431b858f6876b1b06a3036edd8e931ef8e813
Norberto Oliveira Junior <norbejunior@gmail.com>
2021-04-03 12:33:56 -0300

    Add moduledoc for Machinist.Transition

--- lib/machinist/transition.ex
+++ lib/machinist/transition.ex
@@ -1,4 +1,7 @@
 defmodule Machinist.Transition do
+  @moduledoc """
+  `Machinist.Transition` module behaviour
+  """
   @callback transit(struct(), event: String.t()) ::
               {:ok, struct()} | {:error, :not_allowed}
 end
